507012c1f54656adea3c2a93fce40e75ac6b143c
Hiep18 <93771989+Hiep18@users.noreply.github.com>
2021-11-05 20:12:15 +0700

    tao hiep 12

--- src/com/qlpk/dao/NhanVienDao.java
+++ src/com/qlpk/dao/NhanVienDao.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.qlpk.dao;
 
 /**
@@ -10,5 +6,7 @@ package com.qlpk.dao;
  * @author Administrator
  */
 public class NhanVienDao {
-    
+    public static void main(String[] args) {
+        System.out.println("com.qlpk.dao.NhanVienDao.main()");
+    }
 }
