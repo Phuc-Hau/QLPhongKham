13000453222a175f920125e1d875b8fa19b2ed2d
Phuc-Hau <93696783+Phuc-Hau@users.noreply.github.com>
2021-11-06 20:20:32 +0700

    Hau add phieu kham dao + entity

--- src/com/qlpk/dao/PhieuKhamDao.java
+++ src/com/qlpk/dao/PhieuKhamDao.java
@@ -11,7 +11,9 @@ public class PhieuKhamDao extends PhongKhamDao<PhieuKham, Integer>{
 
     @Override
     public void insert(PhieuKham entity) {
-        String Insert ="";
+        String Insert ="INSERT INTO PhieuKham\n" +
+"                      (MaPhieuKham, MABN, MaPK, MaNV, BS, NgayKham, KhamLamSang, BenhKem, ChuanDoanICD, NhipTim, NhietDo, HuyenAp, CanNang, ChieuCao, KetLuan_DieuTri)\n" +
+"VALUES    (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         JdbcHelper.Update(Insert, 
                 entity.getMaPhieuKham(),
                 entity.getMaBN(),
@@ -33,7 +35,9 @@ public class PhieuKhamDao extends PhongKhamDao<PhieuKham, Integer>{
 
     @Override
     public void update(PhieuKham entity) {
-        String Update ="";
+        String Update ="UPDATE    PhieuKham\n" +
+"SET             MABN =?, MaPK =?, MaNV =?, BS =?, NgayKham =?, KhamLamSang =?, BenhKem =?, ChuanDoanICD =?, NhipTim =?, NhietDo =?, HuyenAp =?, CanNang =?, ChieuCao =?, KetLuan_DieuTri =? \n" +
+"WHERE     (MaPhieuKham = ?)";
         JdbcHelper.Update(Update, 
                 entity.getMaBN(),
                 entity.getMaPK(),
@@ -55,12 +59,15 @@ public class PhieuKhamDao extends PhongKhamDao<PhieuKham, Integer>{
 
     @Override
     public void detele(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String Delete ="DELETE FROM PhieuKham WHERE (MaPhieuKham = ?";
+        JdbcHelper.Update(Delete, id);
     }
 
     @Override
     public PhieuKham selectByID(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String SelectID ="SELECT    PhieuKham.* FROM hieuKham WHERE (MaPhieuKham = ?)";
+        List<PhieuKham> list =selectBySql(SelectID, id);
+        return list.size()>0?list.get(0):null;
     }
 
     @Override
