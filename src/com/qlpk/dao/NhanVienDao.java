f6426aebb2286bb505ff30048f6b668542fd1269
Hiep18 <93771989+Hiep18@users.noreply.github.com>
2021-11-06 12:33:01 +0700

    hiep up nhanvien

--- src/com/qlpk/dao/NhanVienDao.java
+++ src/com/qlpk/dao/NhanVienDao.java
@@ -6,7 +6,5 @@ package com.qlpk.dao;
  * @author Administrator
  */
 public class NhanVienDao {
-    public static void main(String[] args) {
-        System.out.println("com.qlpk.dao.NhanVienDao.main()");
-    }
+   
 }
