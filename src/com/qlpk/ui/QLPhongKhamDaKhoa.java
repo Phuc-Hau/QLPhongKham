8e692b95ecf2ff15a0c049b755842eb2424d25f0
Phuc-Hau <93696783+Phuc-Hau@users.noreply.github.com>
2021-11-07 20:21:57 +0700

    Hậu add Chao

--- src/com/qlpk/ui/QLPhongKhamDaKhoa.java
+++ src/com/qlpk/ui/QLPhongKhamDaKhoa.java
@@ -9,12 +9,12 @@ package com.qlpk.ui;
  *
  * @author fptshop
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class QLPhongKhamDaKhoa extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form QLPhongKhamDaKhoa
      */
-    public NewJFrame() {
+    public QLPhongKhamDaKhoa() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QLPhongKhamDaKhoa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QLPhongKhamDaKhoa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QLPhongKhamDaKhoa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QLPhongKhamDaKhoa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new QLPhongKhamDaKhoa().setVisible(true);
             }
         });
     }
