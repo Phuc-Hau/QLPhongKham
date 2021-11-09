c6506cd58ac8323f44fb78ff875aa5bf1aa0d8dc
Phuc-Hau <93696783+Phuc-Hau@users.noreply.github.com>
2021-11-09 19:32:12 +0700

    Hau add QLPK

--- src/com/qlpk/ui/QLPhongKhamDaKhoa.java
+++ src/com/qlpk/ui/QLPhongKhamDaKhoa.java
@@ -10,7 +10,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Calendar; 
 import java.util.Date;
 import javax.swing.JPanel;
 import javax.swing.Timer;
