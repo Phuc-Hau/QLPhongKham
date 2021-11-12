ccc30a734945f844bb899e9e92079bb68535a2d6
Huy PC <Huy PC@DESKTOP-T30CCVU>
2021-11-12 13:30:12 +0700

    Huy sửa dangnhap

--- src/com/qlpk/ui/DangNhapJDialong.java
+++ src/com/qlpk/ui/DangNhapJDialong.java
@@ -84,6 +84,7 @@ public class DangNhapJDialong extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Đăng nhâp");
 
         jPanel2.setBackground(new java.awt.Color(204, 204, 255));
 
