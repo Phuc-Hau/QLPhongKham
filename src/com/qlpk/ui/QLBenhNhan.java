a9ad00c0a1fdfe7121e20bbd5a61787357a424b1
Phuc-Hau <93696783+Phuc-Hau@users.noreply.github.com>
2021-12-10 19:33:42 +0700

    h

--- src/com/qlpk/ui/QLBenhNhan.java
+++ src/com/qlpk/ui/QLBenhNhan.java
@@ -234,6 +234,7 @@ public class QLBenhNhan extends javax.swing.JPanel {
         lblNgayTao.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         lblNgayTao.setText("Ngày tạo :");
 
+        txtNgayTao.setEditable(false);
         txtNgayTao.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         txtNgayTao.setForeground(new java.awt.Color(255, 0, 183));
         txtNgayTao.setHorizontalAlignment(javax.swing.JTextField.TRAILING);
